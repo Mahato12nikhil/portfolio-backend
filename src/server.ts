8d11cb5675a229340f08d736ab92d90116f9e565
Nikhil Mahato <nikhilmahato274@gmail.com>
2025-03-02 16:20:30 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ const server = Fastify({logger: true});
 server.register(app,{
     isAwesomeApp:true
  });
-server.listen({port: Number(process.env.PORT) || 5000}).then(() => {
+server.listen({port: Number(process.env.PORT) || 5000, host: '0.0.0.0'}).then(() => {
   server.log.info('Server has started successfully...');
 });
